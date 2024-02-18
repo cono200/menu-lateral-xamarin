01da4294df4a8391466f555b75afcd07dc3cfe49
cono200 <tatattian04@gmail.com>
2024-02-18 14:24:18 -0700

    Agregar archivos de proyecto.

---
+++ IntentoMenuStoreCone/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using IntentoMenuStoreCone.Vistas;
+
+namespace IntentoMenuStoreCone
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Master = new navegacion();
+            this.Detail = new NavigationPage(new IniciarSesion());
+            App.MasterDet = this;
+        }
+    }
+}
